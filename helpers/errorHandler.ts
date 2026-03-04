5956bdcb7fd52363fc113f726a38fc2d3986e8c2
Nazarii Kaliuzhnyi <kaliuzhnyinazarii@gmail.com>
2026-03-04 17:59:11 +0100

    update helpers/errorHandler.ts

---
+++ helpers/errorHandler.ts
@@ -0,0 +1,19 @@
+const errorMessages: Record<number, string> = {
+  400: "Bad request!",
+  401: "Unauthorized!",
+  403: "Forbidden!",
+  404: "Not found!",
+  409: "Conflict!",
+  500: "Server Error",
+};
+
+export type CustomeError extends Error = {
+  status: number;
+}
+
+const errorHandler = (code: number; message = errorMessages[code])=> {
+  const error = new Error();
+  error.message = message;
+  (error as CustomError).status = code;
+  return error;
+}
