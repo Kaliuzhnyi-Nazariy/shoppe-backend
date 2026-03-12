bda9a017b04ae8802594205d8e1460a7225c4e05
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-03-12 12:22:44 +0100

    gathered all routes in one file

---
+++ routes/index.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+
+import authRoutes from "./auth";
+import userRoutes from "./user";
+import productRoutes from "./product";
+import orderRoutes from "./order";
+import { errorHandler, notFoundRouteHandler } from "../handlers";
+
+const router = Router();
+
+router.use("/api/auth", authRoutes);
+
+router.use("/api/users", userRoutes);
+
+router.use("/api/products", productRoutes);
+
+router.use("/api/orders", orderRoutes);
+
+router.use(notFoundRouteHandler);
+
+router.use(errorHandler);
+
+export default router;
