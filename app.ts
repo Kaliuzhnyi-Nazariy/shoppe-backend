fe107f094f0db9fef975df0fab94073f3ef87d6d
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-05-08 20:45:18 +0200

    app config and middlewares

--- app.ts
+++ app.ts
@@ -5,15 +5,15 @@ import routes from "./routes";
 
 const app = express();
 
-app.use(express.json());
-app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
-    credentials: true,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+    origin: ["http://localhost:5173", "https://shoppe-8f1f.vercel.app"],
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
+
+app.use(express.json());
 
 app.use(routes);
 
