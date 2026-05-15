d9ab5b6c18a22dbf35a2540eef2af41cddb1b528
Kaliuzhnyi-Nazariy <132183299+Kaliuzhnyi-Nazariy@users.noreply.github.com>
2026-05-15 13:36:59 +0200

    add auth tests

--- tests/unit/auth.service.test.ts
+++ tests/unit/auth.service.test.ts
@@ -1,192 +1,220 @@
-// import { IUser, SignIn, SignUp } from "../../interfaces/user";
-// import service from "../../service/auth";
-// import { prisma } from "../../lib/prisma";
-// import bcrypt from "bcryptjs";
-// import jwt from "jsonwebtoken";
-// import { jwtVerify } from "jose";
+import { IUser, SignIn, SignUp } from "../../interfaces/user";
+import service from "../../service/auth";
+import { prisma } from "../../lib/prisma";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+// import Mailjet from "node-mailjet";
+// import pLimit from "p-limit";
+import { jwtVerify } from "jose";
 
-// jest.mock("../../lib/prisma", () => ({
-//   prisma: {
-//     user: {
-//       findUnique: jest.fn(),
-//       create: jest.fn(),
-//       update: jest.fn(),
-//     },
-//   },
-// }));
+jest.mock("../../lib/prisma", () => ({
+  prisma: {
+    user: {
+      findUnique: jest.fn(),
+      create: jest.fn(),
+      update: jest.fn(),
+    },
+    passwordResetTokens: {
+      findUnique: jest.fn(),
+      delete: jest.fn(),
+    },
+  },
+}));
 
-// jest.mock("bcryptjs");
-// jest.mock("jsonwebtoken");
+jest.mock("bcryptjs");
+jest.mock("jsonwebtoken");
 
-// jest.mock("jose", () => ({
-//   jwtVerify: jest.fn(),
-// }));
+jest.mock("jose", () => ({
+  jwtVerify: jest.fn(),
+}));
 
-// const mockedPrisma = prisma as jest.Mocked<typeof prisma>;
+jest.mock("node-mailjet", () => ({
+  apiConnect: jest.fn(),
+}));
 
-// // sign up
+const mockedPrisma = prisma as jest.Mocked<typeof prisma>;
 
-// describe("authService.signUp", () => {
-//   const signUpData: SignUp = {
-//     firstName: "Jhon",
-//     lastName: "Doe",
-//     displayName: "sth",
-//     email: "test@test.com",
-//     password: "password123",
-//     role: "customer",
-//   };
+// sign up
 
-//   const mockUser = {
-//     id: "123",
-//     role: "customer",
-//   };
+describe("authService.signUp", () => {
+  const signUpData: SignUp = {
+    firstName: "Jhon",
+    lastName: "Doe",
+    displayName: "sth",
+    email: "test@test.com",
+    password: "password123",
+    role: "customer",
+  };
 
-//   beforeEach(() => {
-//     jest.clearAllMocks();
-//   });
+  const mockUser = {
+    id: "123",
+    role: "customer",
+  };
 
-//   it("should create a user and return token", async () => {
-//     (prisma.user.findUnique as jest.Mock).mockResolvedValue(null);
-//     (bcrypt.hash as jest.Mock).mockResolvedValue("hashed_password");
-//     (prisma.user.create as jest.Mock).mockResolvedValue(mockUser);
-//     (jwt.sign as jest.Mock).mockReturnValue("mock_token");
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-//     const result = await service.signUp(signUpData);
+  it("should create a user and return token", async () => {
+    (prisma.user.findUnique as jest.Mock).mockResolvedValue(null);
+    (bcrypt.hash as jest.Mock).mockResolvedValue("hashed_password");
+    (prisma.user.create as jest.Mock).mockResolvedValue(mockUser);
+    (jwt.sign as jest.Mock).mockReturnValue("mock_token");
 
-//     expect(prisma.user.findUnique).toHaveBeenCalledWith({
-//       where: { email: signUpData.email },
-//     });
+    const result = await service.signUp(signUpData);
 
-//     expect(bcrypt.hash).toHaveBeenCalledWith(signUpData.password, 10);
+    expect(prisma.user.findUnique).toHaveBeenCalledWith({
+      where: { email: signUpData.email },
+    });
 
-//     expect(prisma.user.create).toHaveBeenCalled();
+    expect(bcrypt.hash).toHaveBeenCalledWith(signUpData.password, 10);
 
-//     expect(jwt.sign).toHaveBeenCalled();
+    expect(prisma.user.create).toHaveBeenCalled();
 
-//     expect(result).toEqual({ token: "mock_token" });
-//   });
+    expect(jwt.sign).toHaveBeenCalled();
 
-//   it("should throw error if email exists", async () => {
-//     (prisma.user.findUnique as jest.Mock).mockResolvedValue(mockUser);
-//     await expect(service.signUp(signUpData)).rejects.toThrow();
-//     expect(prisma.user.create).not.toHaveBeenCalled();
-//   });
-// });
+    expect(result).toEqual({ token: "mock_token" });
+  });
 
-// // sign in
+  it("should throw error if email exists", async () => {
+    (prisma.user.findUnique as jest.Mock).mockResolvedValue(mockUser);
+    await expect(service.signUp(signUpData)).rejects.toThrow();
+    expect(prisma.user.create).not.toHaveBeenCalled();
+  });
+});
 
-// describe("authService.signIn", () => {
-//   const userData = {
-//     firstName: "Jhon",
-//     lastName: "Doe",
-//     displayName: "sth",
-//     email: "test@test.com",
-//     password: "password123",
-//     role: "customer",
-//     id: "123",
-//   };
+// sign in
 
-//   const signInData: SignIn = {
-//     email: "test@mail.com",
-//     password: "password123",
-//   };
+describe("authService.signIn", () => {
+  const userData = {
+    firstName: "Jhon",
+    lastName: "Doe",
+    displayName: "sth",
+    email: "test@test.com",
+    password: "password123",
+    role: "customer",
+    id: "123",
+  };
 
-//   beforeEach(() => {
-//     jest.clearAllMocks();
-//   });
+  const signInData: SignIn = {
+    email: "test@mail.com",
+    password: "password123",
+  };
 
-//   it("should return token if user exists and credentials are valid", async () => {
-//     (prisma.user.findUnique as jest.Mock).mockResolvedValue(userData);
-//     (bcrypt.compare as jest.Mock).mockResolvedValue(true);
-//     (jwt.sign as jest.Mock).mockReturnValue("mock_token");
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-//     const result = await service.signIn(signInData);
+  it("should return token if user exists and credentials are valid", async () => {
+    (prisma.user.findUnique as jest.Mock).mockResolvedValue(userData);
+    (bcrypt.compare as jest.Mock).mockResolvedValue(true);
+    (jwt.sign as jest.Mock).mockReturnValue("mock_token");
 
-//     expect(prisma.user.findUnique).toHaveBeenCalledWith({
-//       where: { email: signInData.email },
-//     });
+    const result = await service.signIn(signInData);
 
-//     expect(bcrypt.compare).toHaveBeenCalledWith(
-//       signInData.password,
-//       userData.password,
-//     );
+    expect(prisma.user.findUnique).toHaveBeenCalledWith({
+      where: { email: signInData.email },
+    });
 
-//     expect(jwt.sign).toHaveBeenCalled();
+    expect(bcrypt.compare).toHaveBeenCalledWith(
+      signInData.password,
+      userData.password,
+    );
 
-//     expect(result).toEqual({ token: "mock_token" });
-//     // expect(result).toEqual({ token: "mock_token", role: "customer" });
-//   });
+    expect(jwt.sign).toHaveBeenCalled();
 
-//   it("should throw error because user is not exist", async () => {
-//     (prisma.user.findUnique as jest.Mock).mockResolvedValue(null);
-//     await expect(service.signIn(signInData)).rejects.toThrow();
-//     expect(jwt.sign).not.toHaveBeenCalled();
-//   });
+    expect(result).toEqual({ token: "mock_token" });
+    // expect(result).toEqual({ token: "mock_token", role: "customer" });
+  });
 
-//   it("should throw error because user password is not match with comming one", async () => {
-//     (prisma.user.findUnique as jest.Mock).mockResolvedValue(userData);
-//     (bcrypt.compare as jest.Mock).mockReturnValue(false);
+  it("should throw error because user is not exist", async () => {
+    (prisma.user.findUnique as jest.Mock).mockResolvedValue(null);
+    await expect(service.signIn(signInData)).rejects.toThrow();
+    expect(jwt.sign).not.toHaveBeenCalled();
+  });
 
-//     await expect(service.signIn(signInData)).rejects.toThrow();
-//     expect(jwt.sign).not.toHaveBeenCalled();
-//   });
-// });
+  it("should throw error because user password is not match with comming one", async () => {
+    (prisma.user.findUnique as jest.Mock).mockResolvedValue(userData);
+    (bcrypt.compare as jest.Mock).mockReturnValue(false);
 
-// // reset password
+    await expect(service.signIn(signInData)).rejects.toThrow();
+    expect(jwt.sign).not.toHaveBeenCalled();
+  });
+});
 
-// describe("authService.resetPassword", () => {
-//   // const forgetPasswordValue = {
-//   //   token: "token",
-//   //   password: "password",
-//   //   confirmPassword: "password",
-//   // };
+// reset password
 
-//   const token = "token";
-//   const password = "password";
-//   const confirmPassword = "password";
+describe("authService.resetPassword", () => {
+  // const forgetPasswordValue = {
+  //   token: "token",
+  //   password: "password",
+  //   confirmPassword: "password",
+  // };
 
-//   const tokenPayload = {
-//     payload: { id: "123", role: "customer" },
-//   };
+  const token = "token";
+  const password = "password";
+  const confirmPassword = "password";
 
-//   const userData = {
-//     firstName: "Jhon",
-//     lastName: "Doe",
-//     displayName: "sth",
-//     email: "test@test.com",
-//     password: "hashed_password",
-//     role: "customer",
-//     id: "123",
-//   };
+  const tokenPayload = {
+    payload: { id: "123", role: "customer" },
+  };
 
-//   beforeEach(() => {
-//     jest.clearAllMocks();
-//   });
+  const userData = {
+    firstName: "Jhon",
+    lastName: "Doe",
+    displayName: "sth",
+    email: "test@test.com",
+    password: "hashed_password",
+    role: "customer",
+    id: "123",
+  };
 
-//   it("should update user password", async () => {
-//     (jwtVerify as jest.Mock).mockResolvedValue(tokenPayload);
-//     (bcrypt.hash as jest.Mock).mockResolvedValue("hashed_password2");
-//     (prisma.user.update as jest.Mock).mockResolvedValue(userData);
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-//     await service.resetPassword(token, password, confirmPassword);
+  it("should update user password", async () => {
+    (jwtVerify as jest.Mock).mockResolvedValue(tokenPayload);
+    (prisma.passwordResetTokens.findUnique as jest.Mock).mockResolvedValue({
+      id: "token-1",
+      userId: userData.id,
+      token: "token",
+    });
+    (bcrypt.compare as jest.Mock).mockResolvedValue(true);
+    (bcrypt.hash as jest.Mock).mockResolvedValue("hashed_password2");
+    (prisma.user.update as jest.Mock).mockResolvedValue(userData);
+    (prisma.passwordResetTokens.delete as jest.Mock).mockResolvedValue({
+      where: { userId: userData.id },
+    });
 
-//     expect(jwtVerify).toHaveBeenCalledWith(token, expect.any(Uint8Array));
+    await service.resetPassword(token, password, confirmPassword);
 
-//     expect(bcrypt.hash).toHaveBeenCalledWith(confirmPassword, 10);
+    expect(jwtVerify).toHaveBeenCalledWith(token, expect.any(Uint8Array));
 
-//     expect(prisma.user.update).toHaveBeenCalledWith({
-//       where: { id: tokenPayload.payload.id },
-//       data: { password: "hashed_password2" },
-//     });
-//   });
+    expect(prisma.passwordResetTokens.findUnique).toHaveBeenCalledWith({
+      where: { userId: tokenPayload.payload.id },
+    });
 
-//   it("should return an error because token is not valid", async () => {
-//     (jwtVerify as jest.Mock).mockRejectedValue(new Error("Token is not valid"));
+    expect(bcrypt.compare).toHaveBeenCalledWith("token", "token");
 
-//     await expect(
-//       service.resetPassword(token, password, confirmPassword),
-//     ).rejects.toThrow("Token is not valid");
+    expect(bcrypt.hash).toHaveBeenCalledWith(confirmPassword, 10);
 
-//     expect(prisma.user.update).not.toHaveBeenCalled();
-//   });
-// });
+    expect(prisma.user.update).toHaveBeenCalledWith({
+      where: { id: tokenPayload.payload.id },
+      data: { password: "hashed_password2" },
+    });
+    expect(prisma.passwordResetTokens.delete).toHaveBeenCalledWith({
+      where: { userId: tokenPayload.payload.id },
+    });
+  });
+
+  it("should return an error because token is not valid", async () => {
+    (jwtVerify as jest.Mock).mockRejectedValue(new Error("Token is not valid"));
+
+    await expect(
+      service.resetPassword(token, password, confirmPassword),
+    ).rejects.toThrow("Token is not valid");
+
+    expect(prisma.user.update).not.toHaveBeenCalled();
+  });
+});
